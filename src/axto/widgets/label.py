0c6ae8060d64f2d3627386ba9ff15a456a45fc63
Rioxpi <r.rioxpi@gmail.com>
2026-05-21 15:37:18 +0200

    Add label

---
+++ src/axto/widgets/label.py
@@ -0,0 +1,25 @@
+from axto.widgets.base import Widget
+
+class Label(Widget):
+    def __init__(self, x, y, text, color="73"):
+        super().__init__(x,y, width=len(text), height=1)
+        self.text = text
+        self.color = color
+    
+    def draw(self, term):
+        """Render the label widget
+
+        Args:
+            term (Terminal): Terminal instance used for rendering
+        """
+        term.move_cursor(self.x, self.y)
+        term.write(self.text, self.color)
+    
+    def set_text(self, text):
+        """Update the label's text and adjust width accordingly
+
+        Args:
+            text (str): The new text to display in the label
+        """
+        self.text = text
+        self.width = len(text)
--- tests/test.py
+++ tests/test.py
@@ -1,42 +1,13 @@
 from axto.core import Engine
-from axto.widgets.box import Box
-from axto.widgets.button import Button
-from axto.widgets.input import Input
-from axto.scene_manager import SceneManager
-from axto.scene import Scene
+from axto.widgets.label import Label
 
 def main():
     app = Engine()
-    manager = SceneManager(app)
 
-    # Main menu scene
-    menu_scene = Scene()
-    menu_scene.add_widget(Box(2, 2, 30, 8))
+    label = Label(5, 5, "Hello, World!", "73")
+    app.add_widget(label)
     
-    btn_start = menu_scene.add_widget(Button(5, 4, "START GAME"))
-    btn_settings = menu_scene.add_widget(Button(5, 6, "SETTINGS"))
-
-    # Actions for buttons
-    btn_settings.bind("press", lambda: manager.switch_scene("settings"))
-
-    # Settings scene
-    settings_scene = Scene()
-    settings_scene.add_widget(Box(2, 2, 45, 8))
     
-    # Input for player nickname
-    settings_scene.add_widget(Input(5, 4, 20, placeholder="Enter nickname..."))
-    
-    btn_back = settings_scene.add_widget(Button(5, 6, "BACK TO MENU"))
-    
-    # Action: Return to main menu
-    btn_back.bind("press", lambda: manager.switch_scene("menu"))
-
-
-    manager.add_scene("menu", menu_scene)
-    manager.add_scene("settings", settings_scene)
-
-    # Start with the main menu
-    manager.switch_scene("menu")
     app.run()
 
 if __name__ == "__main__":
