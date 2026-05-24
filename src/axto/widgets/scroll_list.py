b078dfaeb64896b736753e0f4c03d3038f061092
Rioxpi <r.rioxpi@gmail.com>
2026-05-24 14:40:51 +0200

    Add scrollList

---
+++ src/axto/widgets/scroll_list.py
@@ -0,0 +1,41 @@
+from axto.widgets.base import Widget
+from axto.keys import Key
+
+class ScrollList(Widget):
+    def __init__(self, x, y, width, height, items=None):
+        super().__init__(x, y, width, height)
+        self.items = items or []
+        self.selected_index = 0
+        self.scroll_offset = 0
+    
+    def on_key(self, key: Key):
+        if not self.items: return
+        
+        if key == Key.UP:
+            if self.selected_index > 0:
+                self.selected_index -= 1
+                if self.selected_index < self.scroll_offset:
+                    self.scroll_offset -= 1
+        elif key == Key.DOWN:
+            if self.selected_index < len(self.items) - 1:
+                self.selected_index += 1
+                if self.selected_index >= self.scroll_offset + self.height:
+                    self.scroll_offset += 1
+    
+    def draw(self, term):
+        visible_items = self.items[self.scroll_offset : self.scroll_offset + self.height]
+        
+        for i, item in enumerate(visible_items):
+            actual_index = self.scroll_offset + i 
+            current_y = self.y + i
+            
+            if actual_index == self.scroll_offset + i and self.selected_index == actual_index:
+                color = '1;32'
+                prefix = '> '
+            else:
+                color = '37'
+                prefix = ' '
+            
+            term.move_cursor(self.x, current_y)
+            line_text = f"{prefix}{item}"[:self.width]
+            term.write(line_text.ljust(self.width), color)
--- tests/test.py
+++ tests/test.py
@@ -1,17 +1,11 @@
 from axto.core import Engine
-from axto.widgets.box import Box
-from axto.widgets.label import Label
-from axto.widgets.input import Input
-from axto.widgets.button import Button
+from axto.widgets.scroll_list import ScrollList
 
 def main():
     app = Engine()
 
-    box = Box(2, 2, 30, 10, border_style="bold")
-    app.add_widget(box)
-
-    button = Button(5, 5, "Click Me")
-    app.add_widget(button)
+    scroll_list = ScrollList(2, 2, 30, 3, items=["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"])
+    app.add_widget(scroll_list)
 
     app.run()
 
